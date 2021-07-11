7d54b3aeee98706f60f3c5599a66f90851843456
yehisel <yehiselburgos@gmail.com>
2021-07-11 19:18:41 -0400

    cuarto commit

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -3,4 +3,10 @@ $(function() {
 
     $('[data-toggle="popover"]').popover();
     $('[data-toggle="tooltip"]').tooltip();
+
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 2000, function(){
+        window.location.hash = hash;
+    });
 })
